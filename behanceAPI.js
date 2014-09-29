e45452a073d3822897d796b7a4510280e525247e
rBurgett <ryan@burgettweb.net>
2014-09-29 15:03:07 -0400

    Linting...

--- behanceAPI.js
+++ behanceAPI.js
@@ -1,25 +1,42 @@
+/*jslint nomen: true */
+/*global $ */
+
 var Portfolio = {
     _url : 'https://www.behance.net/v2/',
     _user : '',
     _key : '',
-    create : function(p) {
+    create : function (p) {
+        'use strict';
         return Object.create(Portfolio).init(p);
     },
-    init : function(p) {
+    init : function (p) {
+        'use strict';
         this._user = p.user || '';
         this._key = p.key || '';
         return this;
     },
-    url : function() {return this._url},
-    user : function() {return this._user},    
-    key : function() {return this._key},
-    portfolioAPIurl : function() {
-        return this._url + 'users/' + this._user + '/projects?api_key=' + this._key
+    url : function () {
+        'use strict';
+        return this._url;
     },
-    projectAPIurl : function(projectId) {
-        return this._url + 'projects/' + projectId + '?api_key=' + this._key
+    user : function () {
+        'use strict';
+        return this._user;
     },
-    getPortfolio : function(callback) {
+    key : function () {
+        'use strict';
+        return this._key;
+    },
+    portfolioAPIurl : function () {
+        'use strict';
+        return this._url + 'users/' + this._user + '/projects?api_key=' + this._key;
+    },
+    projectAPIurl : function (projectId) {
+        'use strict';
+        return this._url + 'projects/' + projectId + '?api_key=' + this._key;
+    },
+    getPortfolio : function (callback) {
+        'use strict';
         var url = this.portfolioAPIurl();
         $.ajax({
             'global': false,
@@ -29,7 +46,8 @@ var Portfolio = {
             'success': callback
         });
     },
-    getProject : function(projectId, callback) {
+    getProject : function (projectId, callback) {
+        'use strict';
         var url = this.projectAPIurl(projectId);
         $.ajax({
             'global': false,
@@ -39,7 +57,8 @@ var Portfolio = {
             'success': callback
         });
     },
-    addPrevNext : function(data) {
+    addPrevNext : function (data) {
+        'use strict';
         var projectsLength = data.projects.length;
         for (var i = 0; i < projectsLength; i = i + 1) {  //add previous and next Ids to each project object to use w/ nav buttons
             if (i > 0) {
